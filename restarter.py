959158a00dc2ef0f733c81217f6c342d609064e5
ImAssertive <imassertive@gmail.com>
2018-07-13 19:44:50 +0100

    wew

--- restarter.py
+++ restarter.py
@@ -21,7 +21,7 @@ class restarter:
     @commands.command()
     @checks.justme()
     async def updatetraa(self, ctx):
-        shellcommand="cd .. && cd traatan && git pull && cd .. traatan && python3 traatan.py"
+        shellcommand = "cd .. && cd traatan && git pull && cd .. traatan && python3 traatan.py"
         description="Updated and restarted traatan!"
         await self.shellfunction(ctx, shellcommand)
 
