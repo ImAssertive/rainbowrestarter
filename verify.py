1c146ff687b2c4bb0ca8708e795f0170da9de223
ImAssertive <imassertive@gmail.com>
2018-07-15 19:25:42 +0100

    wew

--- verify.py
+++ verify.py
@@ -9,7 +9,10 @@ class verifyCog:
     @checks.whitelisted()
     async def verify(self, ctx, user):
         userid = useful.getid(user)
-        await ctx.author.send('```**Welcome, '+ctx.guild.get_member(userid).mention+'!**\n• Please make sure to read <#441971134217322506>\n• To set your profile tags, send @SamBot#5904 a DM with the command *`!info`*\n• Introduce yourself on <#348800438176317441> if you wish\n• Check out our opt-in channels with the command *`?ranks`* on <#348776263986446336>```')
+        if ctx.author.id == 265953808381640704:
+            await ctx.author.send('```**Welcome, ' + ctx.guild.get_member(userid).mention + '!**\n• Please make sure to read <#441971134217322506>\n• To set your profile tags, send @SamBot#5904 a DM with the command *`!info`*\n• Introduce yourself on <#348800438176317441> if you wish\n• Check out our opt-in channels with the command *`?ranks`* on <#348776263986446336>\nPlease enjoy your time with us :tophat:```')
+        else:
+            await ctx.author.send('```**Welcome, '+ctx.guild.get_member(userid).mention+'!**\n• Please make sure to read <#441971134217322506>\n• To set your profile tags, send @SamBot#5904 a DM with the command *`!info`*\n• Introduce yourself on <#348800438176317441> if you wish\n• Check out our opt-in channels with the command *`?ranks`* on <#348776263986446336>```')
         await ctx.author.send("<#331517548636143626> <#331517548636143626> <#331517548636143626> <#331517548636143626> <#331517548636143626> <#331517548636143626> ")
 
     async def on_member_join(self, ctx):
