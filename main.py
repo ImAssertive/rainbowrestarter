c5a3e119335033068963835706f1527d3459e96e
ImAssertive <imassertive@gmail.com>
2019-04-28 23:26:58 +0100

    Grammar changes

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ class mainCog:
     async def verify(self, ctx, user):
         userid = useful.getid(user)
         if ctx.author.id == 265953808381640704:
-            await ctx.author.send('```**Welcome, ' + ctx.guild.get_member(userid).mention + '!**\n• Please make sure to read <#441971134217322506>\n• To set your profile tags, send @SamBot#5904 a DM with the command *`!info`*\n• Introduce yourself on <#348800438176317441> if you wish\n• Check out our opt-in channels with the command *`?ranks`* on <#348776263986446336>\nPlease enjoy your time with us :tophat:```')
+            await ctx.author.send('```**Welcome, ' + ctx.guild.get_member(userid).mention + '!**\n• Please make sure to read <#441971134217322506>\n• To set your profile tags, send @SamBot#5904 a DM with the command *`!info`*\n• Introduce yourself in <#348800438176317441> if you wish\n• Check out our opt-in channels with the command *`?ranks`* in <#348776263986446336>\nPlease enjoy your time with us :tophat:```')
         else:
-            await ctx.author.send('```**Welcome, '+ctx.guild.get_member(userid).mention+'!**\n• Please make sure to read <#441971134217322506>\n• To set your profile tags, send @SamBot#5904 a DM with the command *`!info`*\n• Introduce yourself on <#348800438176317441> if you wish\n• Check out our opt-in channels with the command *`?ranks`* on <#348776263986446336>```')
+            await ctx.author.send('```**Welcome, '+ctx.guild.get_member(userid).mention+'!**\n• Please make sure to read <#441971134217322506>\n• To set your profile tags, send @SamBot#5904 a DM with the command *`!info`*\n• Introduce yourself in <#348800438176317441> if you wish\n• Check out our opt-in channels with the command *`?ranks`* in <#348776263986446336>```')
         await ctx.author.send("<#331517548636143626> <#331517548636143626> <#331517548636143626> <#331517548636143626> <#331517548636143626> <#331517548636143626> ")
 
     @commands.command(hidden=True)
@@ -30,7 +30,7 @@ class mainCog:
 
     async def on_member_join(self, ctx):
         if ctx.guild.id == 331517548636143626:
-            await ctx.guild.get_member(163691476788838401).send('```**Welcome, '+ctx.mention+'!**\n• Please make sure to read <#441971134217322506>\n• To set your profile tags, send @SamBot#5904 a DM with the command *`!info`*\n• Introduce yourself on <#348800438176317441> if you wish\n• Check out our opt-in channels with the command *`?ranks`* on <#348776263986446336>```')
+            await ctx.guild.get_member(163691476788838401).send('```**Welcome, '+ctx.mention+'!**\n• Please make sure to read <#441971134217322506>\n• To set your profile tags, send @SamBot#5904 a DM with the command *`!info`*\n• Introduce yourself in <#348800438176317441> if you wish\n• Check out our opt-in channels with the command *`?ranks`* in <#348776263986446336>```')
             for counter in range(0,4):
                 await ctx.guild.get_member(163691476788838401).send("NEW MEMBER <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960> <#448388368129064960>")
 
